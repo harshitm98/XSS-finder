6ecdd075ca6235ada56da1a905daf98c3d9fa964
harshitm98 <harshitmah98@gmail.com>
2018-10-10 11:20:41 +0530

    Works with 2 websites

--- xss.py
+++ xss.py
@@ -1,20 +1,57 @@
+import selenium
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.common.exceptions import UnexpectedAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException
 
 profile = webdriver.FirefoxProfile('/home/fake_batman_/.mozilla/firefox/x2n55od1.default')
+webdriver.DesiredCapabilities.FIREFOX["unexpectedAlertBehaviour"] = "accept"
 driver = webdriver.Firefox(firefox_profile=profile)
 # url = 'http://testphp.vulnweb.com/'
-url = 'https://www.woodlandworldwide.com/'
+# url = 'https://www.woodlandworldwide.com/'
 destination_url = 'https://www.woodlandworldwide.com/'
-# url = 'http://prompt.ml/0'
+url = 'https://www.lenovo.com/in/en/laptops/c/LAPTOPS'
 driver.get(url)
 html = driver.page_source
 input_counter = 0
 text_counter = 0
 button_counter = 0
 id_name_counter = 0
+reflected_counter = 0
+reflected_tester = "I am batman"
 # [text, 'id', number]
 list_of_input_tags = []
+
+
+def XSS_checker(list_of_names_, button_name):
+    with open("payloads") as file:
+        payloads = file.read().split("\n")
+    number = 0
+    for payload in payloads:
+        driver.get(url)
+        try:
+            for mName in list_of_names_:
+                if id_name_counter == 1:
+                    name_field = driver.find_element_by_id(mName)
+                else:
+                    name_field = driver.find_element_by_name(mName)
+                if driver.current_url == destination_url:
+                    driver.find_element_by_xpath('/html/body/nav/div/form/div').click()
+                name_field.send_keys(payload)
+            if id_name_counter == 1:
+                button_field = driver.find_element_by_id(button_name)
+            else:
+                button_field = driver.find_element_by_name(button_name)
+            button_field.click()
+            number += 1
+            try:
+                driver.switch_to.alert.accept()
+                print("Payload number: {}\tStatus: {}".format(number, "Success"))
+            except NoAlertPresentException:
+                print("Payload number: {}\tStatus: {}".format(number, "Not success"))
+        except Exception:
+            continue
+
+
 while True:
     if html.find('<input') != -1:
         input_tags = html[html.find('<input'): html.find('>', html.find('<input') + 1)]
@@ -52,6 +89,8 @@ while True:
             text_counter += 1
             input_counter += 1
             print("Text field found...")
+        elif input_tags.find("button") != -1 and input_tags.find("clear") != -1:
+            continue
         elif input_tags.find("button") != -1 or input_tags.find("submit") != -1:
             button_id = input_tags.find("name")
             button_text = input_tags[button_id:]
@@ -76,8 +115,8 @@ while True:
         break
 print(list_of_input_tags)
 counter_www = 0
-# TODO: Send payload using this input tags. Check on atleast three websites.
 text_field_counter = 0
+list_of_names = []
 for input_tag in list_of_input_tags:
     if input_tag[1] == 'search' or input_tag[1] == 'text':
         id = input_tag[0]
@@ -90,7 +129,8 @@ for input_tag in list_of_input_tags:
         if driver.current_url == destination_url and counter_www == 0:
             driver.find_element_by_xpath('/html/body/nav/div/form/div').click()
             counter_www = 1
-        nameField.send_keys("hello")
+        nameField.send_keys(reflected_tester)
+        list_of_names.append(name)
     elif input_tag[1] == 'button':
         name = input_tag[2]
         if id_name_counter ==1:
@@ -100,15 +140,21 @@ for input_tag in list_of_input_tags:
         if text_field_counter == input_tag[3]:
             text_field_counter = 0
             buttonField.click()
-            if driver.page_source.find("hello") != -1:
+            if driver.page_source.find(reflected_tester) != -1:
                 print("The value has been reflected...")
-                break
+                # Check for XSS
+                reflected_counter = 1
+                if XSS_checker(list_of_names, name):
+                    break
+                else:
+                    list_of_names.clear()
+                    continue
             else:
                 driver.get(url)
         else:
             print("Wrong button please check...")
 
 
-# TODO: Use automated all kind of scripts in the payload
+
 # TODO: Implement spidering and go to other pages and implement the same
-driver.close()
+
